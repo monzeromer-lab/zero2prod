4a91305833c364ec2e6c0db4010e98bac637ccf7
Monzer Omer <monzer.a.omer@gmail.com>
2024-06-14 17:59:45 +0200

    skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:0").expect("Faield to bind random port.");
+    let port = listener.local_addr().unwrap().port();
+    println!("http://127.0.0.1:{}", format!("{}",port));
+    run(listener)?.await
 }

c1c9e399391d904acd83f9ee268abc3c5d9a621e
Monzer Omer <monzer.a.omer@gmail.com>
2024-06-16 20:42:19 +0200

    fixed github actions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use zero2prod::{configration::get_configration, run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let configration = get_configration().expect("Faield to deserialize settings.");
     let listener = TcpListener::bind("0.0.0.0:0").expect("Faield to bind random port.");
-    let port = listener.local_addr().unwrap().port();
-    println!("http://127.0.0.1:{}", format!("{}",port));
+    println!("http://127.0.0.1:{}", format!("{}", configration.application_port));
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
+use zero2prod::configration::get_configration;
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("0.0.0.0:0").expect("Faield to bind random port.");
     let port = listener.local_addr().unwrap().port();
@@ -26,6 +29,9 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configration = get_configration().expect("Faield to deserialize settings");
+    let connection_settings = configration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_settings).await.expect("Faield to connect to postgres.");
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -38,6 +44,13 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Faield to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+    
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Faield to fetch saved subscripers");
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
